a218dc2f44fe8d3f8aa8c16fceacabd1d4b3d1d0
さくらみこ <lin@sz.cn.eu.org>
2023-10-10 04:59:33 +0300

    Update classwork1.cpp

--- classwork1.cpp
+++ classwork1.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 #include <cmath>
+#include <memory>
 using namespace std;
 
 void printstr(string s)
 {
   cout << s << endl;
 }
-double sum(double * a, double * b)
+
+double sum(const unique_ptr<double>& a, const unique_ptr<double>& b)
 {
   return *a + *b;
 }
-double mul(double * a, double * b)
+
+double mul(const unique_ptr<double>& a, const unique_ptr<double>& b)
 {
   return *a * *b;
 }
-double sub(double * a, double * b)
+
+double sub(const unique_ptr<double>& a, const unique_ptr<double>& b)
 {
   return *a - *b;
 }
@@ -42,23 +46,23 @@ float err_float()
 }
 
 int main() {
-  double a = 0.0,
-         b = 0.0,
-         c = 0.0;
+  unique_ptr<double> a(new double(0.0));
+  unique_ptr<double> b(new double(0.0));
+  unique_ptr<double> c(new double(0.0));
   cout << "Введите а" << endl;
-  cin >> a;
+  cin >> *a;
   cout << "Введите b" << endl;
-  cin >> b;
+  cin >> *b;
   cout << "Введите c" << endl;
-  cin >> c;
+  cin >> *c;
   printstr("Привет мир!");
-  cout << "a = " << a << endl 
-       << "a^2 = " << a * a << endl;
-  cout << "a + b + c = " << a + b + c << endl;
-  cout << "a * b * c = " << a * b * c << endl;  
-  cout << "a + b = " << sum(&a, &b) << endl;
-  cout << "a - b = " << sub(&a, &b) << endl;
-  cout << "a * b = " << mul(&a, &b) << endl;
+  cout << "a = " << *a << endl 
+       << "a^2 = " << *a * *a << endl;
+  cout << "a + b + c = " << *a + *b + *c << endl;
+  cout << "a * b * c = " << *a * *b * *c << endl;  
+  cout << "a + b = " << sum(a, b) << endl;
+  cout << "a - b = " << sub(a, b) << endl;
+  cout << "a * b = " << mul(a, b) << endl;
   cout << "Вывод числе от 1 до 10 с шагом 1:" << endl;
   for (int i = 1; i < 11; ++i)
     cout << i << endl;
